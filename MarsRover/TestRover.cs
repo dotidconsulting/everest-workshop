70de4c39fda5b2a1f97bd607dc6481549a3221da
bobp-id <75350226+bobp-id@users.noreply.github.com>
2023-06-30 11:53:22 +1000

    work in progress

---
+++ MarsRover/TestRover.cs
@@ -0,0 +1,39 @@
+﻿namespace MarsRover;
+
+public enum Direction
+{
+    N,
+    S,
+    E,
+    W
+}
+
+public enum Commands
+{
+    f,
+    b
+}
+
+public class TestRover
+{
+    public TestRover(int x, int y, Direction direction)
+    {
+        X = x;
+        Y = y;
+        Direction = direction;
+    }
+
+    public int X { get; set; }
+
+    public int Y { get; set; }
+
+    public Direction Direction { get; set; }
+
+
+    public void InputCommands(Commands[] commands)
+    {
+        foreach (var command in commands)
+            if (command == Commands.f)
+                Y = Y + 1;
+    }
+}
---
+++ MarsRoverTests/TestRover.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using MarsRover;
+using Xunit;
+
+namespace MarsRoverTests;
+
+public class TestRoverSuite
+{
+    [Fact]
+    public void TestRoverFunction()
+    {
+        var t = new TestRover(0, 0, Direction.E);
+
+        t.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void TestRoverInitial()
+    {
+        var expectedX = 0;
+        var expectedY = 0;
+        var expectedDirection = Direction.E;
+        var rover = new TestRover(expectedX, expectedY, expectedDirection);
+
+        rover.X.Should().Be(expectedX);
+        rover.Y.Should().Be(expectedY);
+        rover.Direction.Should().Be(expectedDirection);
+    }
+
+    [Fact]
+    public void TestMoveCommand()
+    {
+        var rover = new TestRover(0, 0, Direction.E);
+        
+        rover.InputCommands(new Commands [] { Commands.b, Commands.f});
+    }
+
+
+    [Fact]
+    public void TestRoverReceiveCommands()
+    {
+        var expectedX = 0;
+        var initialY = 0;
+        var expectedY = initialY + 1;
+
+        var expectedDirection = Direction.N;
+
+        var rover = new TestRover(expectedX, initialY, expectedDirection);
+        rover.InputCommands(new Commands[] { Commands.f});
+
+        rover.Direction.Should().Be(expectedDirection);
+        rover.X.Should().Be(expectedX);
+        rover.Y.Should().Be(expectedY);
+    }
+}
