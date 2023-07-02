42030c4531b921a16576975dc0e8234261a66a34
zeroone <bruce.cauchi@outlook.com>
2023-07-03 09:03:39 +1000

    removed weather  controller and added MarRover object and tests

---
+++ EverestAPI/MarsRover.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverestAPI
+{
+
+    public class Obstacle
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Obstacle(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    public class ObstacleList
+    {
+        public List<Obstacle> Obstacles { get; private set; } = new List<Obstacle>();
+        public ObstacleList() { }
+        public void AddObstacle(int x, int y)
+        {
+            this.Obstacles.Add(new Obstacle(x, y));
+        }
+        public bool IsHit(int x, int y)
+        {
+            return this.Obstacles.Exists(d => d.X == x && d.Y == y);
+        }
+    }
+
+    public class MarsRover
+    {
+
+
+        private ObstacleList obstacleList = new ObstacleList();
+        private int maxX = 10;
+        private int maxY = 10;
+        private List<string> compass = new List<string> { "N", "E", "S", "W" };
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public string Direction { get; private set; }
+        public MarsRover(int x, int y, string direction)
+        {
+            X = x;
+            Y = y;
+            Direction = direction;
+
+            // Add an obstacle
+            obstacleList.AddObstacle(1, 1);
+        }
+
+        public void execute(string[] commands)
+        {
+            foreach (var command in commands)
+            {
+                executeCommand(command);
+            }
+
+        }
+
+        public void executeCommand(string commandName)
+        {
+            int newX = X;
+            int newY = Y;
+            switch (commandName)
+            {
+                case "F": (newX, newY) = moveForward(); break;
+                case "B": (newX, newY) = moveBackward(); break;
+                case "L": turnLeft(); break;
+                case "R": turnRight(); break;
+            }
+
+            // Check whether the new coordindate hits an obstacle.
+            if (obstacleList.IsHit(newX, newY))
+                throw new Exception($"Obstacle at x:{newX}, y:{newY} hit! Self destructing");
+
+
+            // If we have reach here, we update the new coordinates of the Mars Rover
+            Y = newY; X = newX;
+        }
+
+        private (int x, int y) moveForward()
+        {
+            var newY = Y;
+            var newX = X;
+
+            switch (Direction)
+            {
+                case "N":
+                    newY++;
+                    if (newY >= maxY) newY = 0;
+                    break;
+
+                case "S":
+                    newY--;
+                    if (newY <= -1) newY = maxY - 1;
+                    break;
+                case "E":
+                    newX++;
+                    if (newX >= maxX) newX = 0;
+                    break;
+                case "W":
+                    newX--;
+                    if (newX <= -1) newX = maxX - 1;
+                    break;
+            }
+
+            return (newX, newY);
+
+
+        }
+        private (int x, int y) moveBackward()
+        {
+            var newY = Y;
+            var newX = X;
+
+            switch (Direction)
+            {
+                case "N":
+                    newY--;
+                    if (newY <= -1) newY = maxY - 1;
+                    break;
+                case "S":
+                    newY++;
+                    if (newY >= maxY) newY = 0;
+                    break;
+                case "E":
+                    newX--;
+                    if (newX <= -1) newX = maxX - 1;
+                    break;
+                case "W":
+                    newX++;
+                    if (newX >= maxX) newX = 0;
+                    break;
+            }
+
+            return (newX, newY);
+
+
+        }
+
+        public void turnLeft()
+        {
+            var currentDirection = compass.IndexOf(this.Direction);
+            if (currentDirection == 0)
+            {
+                this.Direction = compass[3];
+                return;
+            }
+            this.Direction = compass[currentDirection - 1];
+        }
+
+        public void turnRight()
+        {
+            var currentDirection = compass.IndexOf(this.Direction);
+            if (currentDirection == 3)
+            {
+                this.Direction = compass[0];
+                return;
+            }
+            this.Direction = compass[currentDirection + 1];
+        }
+
+    }
+}
---
+++ EverestAPITests/RoverTests.cs
@@ -0,0 +1,93 @@
+﻿using EverestAPI;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverestAPITests
+{
+    public class RoverTests
+    {
+        [Fact]
+        public void MarRover_Can_Initialise_With_Coord_Directions()
+        {
+            var rover = new MarsRover(0, 0, "N");
+            rover.X.Should().Be(0);
+            rover.Y.Should().Be(0);
+            rover.Direction.Should().Be("N");
+        }
+
+        [Fact]
+        public void MarRover_Can_Accept_command()
+        {
+            var rover = new MarsRover(0, 0, "N");
+            rover.execute(new string[] { "F" });
+        }
+        [Fact]
+        public void MarsRover_Can_Go_Forward_In_North()
+        {
+            var rover = new MarsRover(0, 0, "N");
+            rover.execute(new string[] { "F" });
+            rover.Y.Should().Be(1);
+
+        }
+
+        [Theory]
+        [InlineData("N", "R", "E")]
+        [InlineData("E", "R", "S")]
+        [InlineData("S", "R", "W")]
+        [InlineData("W", "R", "N")]
+        [InlineData("N", "L", "W")]
+        [InlineData("W", "L", "S")]
+        [InlineData("S", "L", "E")]
+        [InlineData("E", "L", "N")]
+        public void MarsRover_Can_Change_Direction(string currentDirection, string turnDirection, string expectedDirection)
+        {
+            var rover = new MarsRover(0, 0, currentDirection);
+            rover.execute(new string[] { turnDirection });
+            rover.Direction.Should().Be(expectedDirection);
+
+        }
+
+        [Theory]
+        [InlineData(0, 1, "F", "N", 0, 2)]
+        [InlineData(0, 1, "B", "N", 0, 0)]
+        [InlineData(1, 1, "F", "E", 2, 1)]
+        [InlineData(1, 1, "B", "E", 0, 1)]
+        [InlineData(1, 1, "F", "W", 0, 1)]
+        [InlineData(1, 1, "B", "W", 2, 1)]
+        [InlineData(0, 1, "F", "S", 0, 0)]
+        [InlineData(0, 1, "B", "S", 0, 2)]
+        [InlineData(0, 9, "F", "N", 0, 0)]
+        [InlineData(0, 0, "B", "N", 0, 9)]
+        [InlineData(0, 0, "F", "S", 0, 9)]
+        [InlineData(0, 9, "B", "S", 0, 0)]
+        [InlineData(9, 0, "F", "E", 0, 0)]
+        [InlineData(0, 0, "B", "E", 9, 0)]
+        [InlineData(0, 0, "F", "W", 9, 0)]
+        [InlineData(9, 0, "B", "W", 0, 0)]
+        public void MarsRover_Can_Move(int x, int y, string cmd, string d, int expectedX, int expectedY)
+        {
+            var rover = new MarsRover(x, y, d);
+            rover.execute(new string[] { cmd });
+            rover.Y.Should().Be(expectedY);
+            rover.X.Should().Be(expectedX);
+
+        }
+
+        [Fact]
+        public void MarsRover_Aborts_When_Hitting_Obstacle()
+        {
+            var rover = new MarsRover(0, 0, "N");
+
+            rover.Invoking(y => y.execute(new string[] { "F", "R", "F" }))
+                .Should().Throw<Exception>()
+                .WithMessage("Obstacle at x:1, y:1 hit! Self destructing");
+
+        }
+
+
+    }
+}
